889193e99576e73b871d842ecb76626fea5eb531
Pablo Adames <pablo.adames@gmail.com>
2020-11-09 16:39:30 -0700

    building googletest in the right place(?)

--- watched/test/watched_test.cpp
+++ watched/test/watched_test.cpp
@@ -1,15 +1,15 @@
 #include <gtest/gtest.h>
 #include <string>
 #include <memory>
-#include "../watched.h"
+#include "../Watched.h"
 
 TEST(watchedName, returnsItsName) {
-  const string a_name = "The Great Escape";
-  std::shared_ptr<Watched> w = new Watched(a_name);
-  EXPECT_TRUE(w->name(), a_name)
+  const string& a_name = "The Great Escape";
+  std::shared_ptr<Watched> w(new Watched(a_name));
+  EXPECT_EQ(w->name(), a_name);
 }
 
-int main(int arc, char ** argv) {
-  ::testing::initGoogleTest(&argc, argv);
+int main(int argc, char ** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
