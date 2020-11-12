7fae184c0f41610e1bae4d51eff42aa5b47e5c7a
Pablo Adames <pablo.adames@gmail.com>
2020-11-12 14:36:17 -0700

    using namespace std in main.cpp

--- watched/main.cpp
+++ watched/main.cpp
@@ -4,27 +4,28 @@
 #include "Watched.h"
 #include "Observer.h"
 
+using namespace std;
 
 int main() {
-	std::shared_ptr<Watched> w1(new Watched("The Creole Food Channel"));
-	std::shared_ptr<Watched> w2(new Watched("The Alton Brown Network"));
-	std::unique_ptr<Observer> o1(new Observer("Captain O"));
+	shared_ptr<Watched> w1(new Watched("The Creole Food Channel"));
+	shared_ptr<Watched> w2(new Watched("The Alton Brown Network"));
+	unique_ptr<Observer> o1(new Observer("Captain O"));
 	o1->watch(w1);
 	o1->watch(w2);
 
 	string o1_is_or_not = o1->isWatching(w2)? " is ": " is not ";
-	std::cout << o1->name() << o1_is_or_not << "watching " << w2->name() << std::endl;
+	cout << o1->name() << o1_is_or_not << "watching " << w2->name() << endl;
 
-	std::cout << "Before " << o1->name() << " stops watching " << w2->name() << " he is watching:" << std::endl;
+	cout << "Before " << o1->name() << " stops watching " << w2->name() << " he is watching:" << endl;
 	
 	o1->printWatched();
 
 	o1->unwatch(w2);
 
 	o1_is_or_not = o1->isWatching(w2)? " is ": " is not ";
-	std::cout << o1->name() << o1_is_or_not << "watching " << w2->name() << std::endl;
+	cout << o1->name() << o1_is_or_not << "watching " << w2->name() << endl;
 
-	std::cout << o1->name() << " is watching: " << std::endl;
+	cout << o1->name() << " is watching: " << endl;
 	o1->printWatched();
 
 }
